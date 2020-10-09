90defabfdd3e3d3c2eb62e0b56f40689d995ab57
João Paulo Vieira da Silva <scrocrix@gmail.com>
2020-10-09 22:46:08 +0400

    Update insertionsort.go

--- insertionsort/insertionsort.go
+++ insertionsort/insertionsort.go
@@ -1,12 +1,21 @@
 package insertionsort
 
+import "errors"
+
+var (
+	SortOrderAscending  = "asc"
+	SortOrderDescending = "desc"
+
+	ErrEmptyParameter = "parameter must not be empty"
+)
+
 type InsertionSort interface {
-	// SortNumbers return an indefinite slice of descending ordered numbers
-	SortNumbers(numbers []int) []int
+	SortNumbers(numbers []int) ([]int, error)
 }
 
 type insertionSort struct {
 	InsertionSort
+	numbers []int
 }
 
 // NewInsertionSort initializes the insertionSort struct for sorting
@@ -14,17 +23,38 @@ func NewInsertionSort() *insertionSort {
 	return &insertionSort{}
 }
 
-func (i *insertionSort) SortNumbers(numbers []int) []int {
-	for key, number := range numbers {
-		i := key - 1
-
-		for i >= 0 && numbers[i] > number {
-			numbers[i + 1] = numbers[i]
-			i = i - 1
-		}
-
-		numbers[i + 1] = number
+// SortNumbers return an indefinite slice of descending ordered numbers
+func (i *insertionSort) SortNumbers(numbers []int, orderBy string) ([]int, error) {
+	if len(orderBy) == 0 {
+		return nil, errors.New(ErrEmptyParameter)
 	}
 
-	return numbers
+	i.numbers = numbers
+
+	for key, number := range numbers {
+		o := i.sort(orderBy, key, number)
+		numbers[o+1] = number
+	}
+
+	return numbers, nil
+}
+
+func (i *insertionSort) sort(order string, key int, number int) int {
+	o := key - 1
+
+	if order == SortOrderAscending {
+		for o >= 0 && i.numbers[o] > number {
+			i.numbers[o+1] = i.numbers[o]
+			o = o - 1
+		}
+	}
+
+	if order == SortOrderDescending {
+		for o >= 0 && i.numbers[o] < number {
+			i.numbers[o+1] = i.numbers[o]
+			o = o - 1
+		}
+	}
+
+	return o
 }
--- insertionsort/insertionsort_test.go
+++ insertionsort/insertionsort_test.go
@@ -11,15 +11,36 @@ type insertionSortSuite struct {
 	suite.Suite
 }
 
-func (suite *insertionSortSuite) TestReturnSortedArrayInDescendingOrder() {
+func (suite *insertionSortSuite) TestSortNumbersReturnSliceOfIntegersInAscendingOrder() {
 	sut := insertionsort.NewInsertionSort()
 
 	un := []int{5, 4, 2, 6, 1, 3}
-	result := sut.SortNumbers(un)
+	result, _ := sut.SortNumbers(un, insertionsort.SortOrderAscending)
 
 	assert.NotEmpty(suite.T(), result)
 	assert.Equal(suite.T(), len(un), len(result))
 	assert.Equal(suite.T(), []int{1, 2, 3, 4, 5, 6}, result)
+}
+
+func (suite *insertionSortSuite) TestSortNumbersReturnSliceOfIntegersInDescendingOrder() {
+	sut := insertionsort.NewInsertionSort()
+
+	un := []int{5, 4, 2, 6, 1, 3}
+	result, _ := sut.SortNumbers(un, insertionsort.SortOrderDescending)
+
+	assert.NotEmpty(suite.T(), result)
+	assert.Equal(suite.T(), len(un), len(result))
+	assert.Equal(suite.T(), []int{6, 5, 4, 3, 2, 1}, result)
+}
+
+func (suite *insertionSortSuite) TestSortNumbersReturnErrorWhenOrderByParameterIsEmpty() {
+	sut := insertionsort.NewInsertionSort()
+
+	un := []int{5, 4, 2, 6, 1, 3}
+	_, err := sut.SortNumbers(un, "")
+
+	assert.NotNil(suite.T(), err)
+	assert.Equal(suite.T(), insertionsort.ErrEmptyParameter, err.Error())
 }
 
 func TestInsertionSortSuite(t *testing.T) {
