55dedd004832e6726e77db19d53951e0ab003c18
João Paulo Vieira da Silva <scrocrix@gmail.com>
2020-10-11 16:33:42 +0400

    Add open site grid functionality to percolation

--- percolation/percolation.go
+++ percolation/percolation.go
@@ -1,11 +1,37 @@
 package percolation
 
-import "errors"
+import (
+	"errors"
+	"math/rand"
+)
+
+// Site represents a single cell from the percolation's grid.
+type Site struct {
+	Row int
+	Column int
+	IsOpened bool
+	IsBlocked bool
+	Position int
+}
+
+// Validate check if the arguments in given to Site is correct
+func (s *Site) Validate () error {
+	if s.Row <= 0 || s.Column <= 0 {
+		return errors.New("error: either row or column arguments must be greater than zero")
+	}
+
+	if s.Position < 0 {
+		return errors.New("error: site must not have a negative position")
+	}
+
+	return nil
+}
+
 
 type percolation struct {
 	rows int
 	columns int
-	sites [][2]int
+	sites []Site
 }
 
 // NewPercolation creates a new instances of percolation as well as, configures
@@ -29,14 +55,14 @@ func (p *percolation) buildSites() error {
 		return errors.New("error: row and column must be equal in order to create a quadratic percolation")
 	}
 
-	var sites [][2]int
+	var sites []Site
 
 	currentRow := 1
 
 	currentColumn := 1
 
 	for siteCount := 0; siteCount < p.rows * p.columns; siteCount++ {
-		var site [2]int
+		var site Site
 
 		// verify if we must break to a new row
 		if siteCount % p.rows == 0 {
@@ -46,8 +72,14 @@ func (p *percolation) buildSites() error {
 			currentColumn = 1
 		}
 
-		site[0] = currentRow
-		site[1] = currentColumn
+		site.Row = currentRow
+		site.Column = currentColumn
+		site.IsOpened = false
+		site.Position = siteCount
+
+		// randomly assign a blocked site to the percolation
+		site.IsBlocked = rand.Int() % 2 == 0
+
 		currentColumn++
 
 		sites = append(sites, site)
@@ -59,20 +91,26 @@ func (p *percolation) buildSites() error {
 }
 
 // GetSites return the sites that have been initialized with the percolation.
-func (p *percolation) GetSites() [][2]int {
+func (p *percolation) GetSites() []Site {
 	return p.sites
 }
 
-// Open finds the site in the grid based on the coordinates and verifies if
-// the spot must or must not be opened.
-func (p *percolation) Open(row int, col int) (bool, error) {
-	if row <= 0 || col <= 0 {
-		return false, errors.New("error: either row or column arguments must be greater than zero")
+// Open finds the site in the grid based on the coordinates and verifies if the spot must or must not be opened.
+func (p *percolation) Open(site Site) (bool, error) {
+	if err := site.Validate(); err != nil {
+		return false, err
 	}
 
-	if row > p.rows || col > p.columns {
-		return false, errors.New("error: either row or column are greater than total sites count")
+	if site.Position > len(p.sites) || p.sites[site.Position] != site {
+		return false, errors.New("error: the given site does not match any of the percolation's")
 	}
 
-	return true, nil
+	if site.IsOpened || site.IsBlocked {
+		return false, nil
+	}
+
+	site.IsOpened = true
+	p.sites[site.Position] = site
+
+	return site.IsOpened, nil
 }
--- percolation/percolation_test.go
+++ percolation/percolation_test.go
@@ -27,44 +27,103 @@ func (suite *percolationSuite) TestNewPercolationReturnErrorWhenSiteSizeDoesNotM
 	assert.Equal(suite.T(), "error: row and column must be equal in order to create a quadratic percolation", err.Error())
 }
 
-func (suite *percolationSuite) TestOpenReturnTrueWhenSiteHasBeenOpened() {
+func (suite *percolationSuite) TestOpenReturnTrueWhenSiteHasJustBeenOpened() {
 	sut, _ := percolation.NewPercolation(5, 5)
 
-	result, _ := sut.Open(1, 2)
+	s := sut.GetSites()[2]
+
+	result, _ := sut.Open(s)
 
 	assert.True(suite.T(), result)
+}
+
+func (suite *percolationSuite) TestOpenReturnFalseWhenSiteHasAlreadyBeenOpened() {
+	sut, _ := percolation.NewPercolation(5, 5)
+
+	result, _ := sut.Open(sut.GetSites()[0])
+
+	assert.True(suite.T(), result)
+
+	result, _ = sut.Open(sut.GetSites()[0])
+
+	assert.False(suite.T(), result)
+}
+
+func (suite *percolationSuite) TestOpenReturnFalseWhenSiteHasBeenBlocked() {
+	sut, _ := percolation.NewPercolation(5, 5)
+
+	result, _ := sut.Open(percolation.Site{
+		Row: 1,
+		Column: 2,
+		IsOpened: false,
+		IsBlocked: true,
+	})
+
+	assert.False(suite.T(), result)
 }
 
 func (suite *percolationSuite) TestOpenReturnErrorWhenRowOrColumnAreLessThanZero() {
 	sut, _ := percolation.NewPercolation(5, 5)
 
-	isOpened, err := sut.Open(0, 1)
+	isOpened, err := sut.Open(percolation.Site{
+		Row: 0,
+		Column: 1,
+	})
 
 	assert.False(suite.T(), isOpened)
 	assert.Error(suite.T(), err)
 	assert.Equal(suite.T(), "error: either row or column arguments must be greater than zero", err.Error())
 
-	isOpened, err = sut.Open(1, 0)
+	isOpened, err = sut.Open(percolation.Site{
+		Row: 1,
+		Column: 0,
+	})
 
 	assert.False(suite.T(), isOpened)
 	assert.Error(suite.T(), err)
 	assert.Equal(suite.T(), "error: either row or column arguments must be greater than zero", err.Error())
 }
 
-func (suite *percolationSuite) TestOpenReturnErrorWhenRowOrColumnAreGreaterThanPercolationSize() {
+func (suite *percolationSuite) TestOpenReturnErrorWhenSiteWasNotFoundInPercolation() {
 	sut, _ := percolation.NewPercolation(5, 5)
 
-	isOpened, err := sut.Open(26, 2)
+	isOpened, err := sut.Open(percolation.Site{
+		Row: 1,
+		Column: 1,
+		Position: 5,
+	})
 
 	assert.False(suite.T(), isOpened)
 	assert.Error(suite.T(), err)
-	assert.Equal(suite.T(), "error: either row or column are greater than total sites count", err.Error())
+	assert.Equal(suite.T(), "error: the given site does not match any of the percolation's", err.Error())
+}
 
-	isOpened, err = sut.Open(3, 12)
+func (suite *percolationSuite) TestOpenReturnErrorWhenSitePositionIsGreaterThanTheTotalPercolationCount() {
+	sut, _ := percolation.NewPercolation(5, 5)
+
+	isOpened, err := sut.Open(percolation.Site{
+		Row: 1,
+		Column: 1,
+		Position: 30,
+	})
 
 	assert.False(suite.T(), isOpened)
 	assert.Error(suite.T(), err)
-	assert.Equal(suite.T(), "error: either row or column are greater than total sites count", err.Error())
+	assert.Equal(suite.T(), "error: the given site does not match any of the percolation's", err.Error())
+}
+
+func (suite *percolationSuite) TestOpenReturnErrorWhenSitePositionIsNegative() {
+	sut, _ := percolation.NewPercolation(5, 5)
+
+	isOpened, err := sut.Open(percolation.Site{
+		Row: 1,
+		Column: 1,
+		Position: -1,
+	})
+
+	assert.False(suite.T(), isOpened)
+	assert.Error(suite.T(), err)
+	assert.Equal(suite.T(), "error: site must not have a negative position", err.Error())
 }
 
 func TestPercolationSuite(t *testing.T) {
